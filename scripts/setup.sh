21c945b1b5c663dd122d058d86ca58f781dd0e6d
Adewale Ekerin <msg4ekerin@yahoo.com>
2023-01-27 17:09:40 +0000

    updated script

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,6 +1,6 @@
 ##Install the needed packages and enable the services(MariaDb, Apache)
 sudo yum update -y
-sudo yum install git -y
+#sudo yum install git -y
 sudo amazon-linux-extras install -y lamp-mariadb10.2-php7.2 php7.2
 sudo yum install -y httpd mariadb-server
 sudo systemctl start httpd
